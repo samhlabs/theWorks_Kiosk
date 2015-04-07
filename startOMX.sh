9b8df94389393c88422c30c7c7bb4eaae7d3e34c
Garrett Martin <garrett@standalone.co>
2015-04-07 18:18:43 -0400

    nohup attempt at getting rid of lxterminal window

--- startOMX.sh
+++ startOMX.sh
@@ -3,5 +3,5 @@
 sudo killall -TERM omxplayer.bin 2>/dev/null;
 sleep .1;
 lxterminal &;
-sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4;
+sudo nohup omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 >/dev/null;
 exit 0
