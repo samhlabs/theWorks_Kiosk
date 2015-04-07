2301c8c23aae741c6abf4e7593f23363fc30a2e6
Garrett Martin <garrett@standalone.co>
2015-04-07 18:38:48 -0400

    added an extra clear and removed the 2 from >/dev/null

--- startOMX.sh
+++ startOMX.sh
@@ -3,6 +3,7 @@
 sudo killall -TERM omxplayer.bin 2>/dev/null
 sleep .1
 lxterminal -e &
-sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 2>/dev/null
+clear
+sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 >/dev/null
 clear
 exit 0
