7309a25fd299644b42c8c913b9c0550c5f1bf01d
Garrett Martin <garrett@standalone.co>
2015-04-07 18:41:48 -0400

    remove lxterminal call and try xterm to launch omxplayer

--- startOMX.sh
+++ startOMX.sh
@@ -2,8 +2,7 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null
 sleep .1
-lxterminal -e &
 clear
-sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 >/dev/null
+xterm -e sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 >/dev/null
 clear
 exit 0
