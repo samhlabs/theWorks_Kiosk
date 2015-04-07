cd1a1f96e7f7680cb5188b5e9bc468917bcd1b57
Garrett Martin <garrett@standalone.co>
2015-04-07 18:31:31 -0400

    added -b to black background, clear to clear terminal and removed all the ;

--- startOMX.sh
+++ startOMX.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-sudo killall -TERM omxplayer.bin 2>/dev/null;
-sleep .1;
-lxterminal &;
-sudo nohup omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 >/dev/null;
+sudo killall -TERM omxplayer.bin 2>/dev/null
+sleep .1
+sudo omxplayer -b -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 2>/dev/null
+clear
 exit 0
