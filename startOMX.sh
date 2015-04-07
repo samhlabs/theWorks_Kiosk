48f7f3d85675fad9500d7b3e67a1d2943555f703
Garrett Martin <garrett@standalone.co>
2015-04-07 17:56:28 -0400

    changing the lxterminal and omxplayer commands

--- startOMX.sh
+++ startOMX.sh
@@ -2,6 +2,6 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null;
 sleep .1;
-lxterminal $;
-sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 >/dev/null;
+lxterminal &;
+sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4;
 exit 0
