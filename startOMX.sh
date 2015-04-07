d0504d28a1a3ac4644f6355940a5d83be648fb19
Garrett Martin <garrett@standalone.co>
2015-04-07 03:46:30 -0400

    remove & so it doesn't push to background

--- startOMX.sh
+++ startOMX.sh
@@ -2,5 +2,5 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null;
 sleep 1;
-print "omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 &";
+print "omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4";
 exit 0
