9fef6320b75c7e92fd9c52b7d44ccc08f36c65ec
Garrett Martin <garrett@standalone.co>
2015-04-07 18:35:43 -0400

    removed the -b (made just a solid black background)... attempting to launch lxterminal in background again

--- startOMX.sh
+++ startOMX.sh
@@ -2,6 +2,7 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null
 sleep .1
-sudo omxplayer -b -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 2>/dev/null
+lxterminal -e &
+sudo omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 2>/dev/null
 clear
 exit 0
