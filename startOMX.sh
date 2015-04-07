38c84c02c36075704dceb0b4b4bdcfb6168494fd
Garrett Martin <garrett@standalone.co>
2015-04-07 03:48:00 -0400

    still not working... trying my options

--- startOMX.sh
+++ startOMX.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 
-sudo killall -TERM omxplayer.bin 2>/dev/null;
-sleep 1;
-print "omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4";
+sudo killall -TERM omxplayer.bin 2>/dev/null && omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4;
 exit 0
