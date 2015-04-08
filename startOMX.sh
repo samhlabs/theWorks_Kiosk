e026498ee2fb38d4733f839d84939158b92a4165
Garrett Martin <garrett@standalone.co>
2015-04-07 22:06:10 -0400

    holy shit I don't know how to use git!

--- startOMX.sh
+++ startOMX.sh
@@ -3,6 +3,6 @@
 sudo killall -TERM omxplayer.bin 2>/dev/null
 sleep .1
 clear
-omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < /tmp/cmdOMX
+omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
 clear
 exit 0
