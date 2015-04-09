57756f3ee94e9604dcf22231aa9a370978435fcc
Garrett Martin <garrett@standalone.co>
2015-04-09 11:57:10 -0400

    push fifo into background

--- startOMX.sh
+++ startOMX.sh
@@ -2,5 +2,5 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null
 sleep .1
-omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < ~/bin/omxfifo
+omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < ~/bin/omxfifo &
 exit 0
