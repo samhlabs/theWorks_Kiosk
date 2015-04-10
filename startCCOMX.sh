812f6b69dfb0f70552112576ca2300f70a15fd04
root <root@kioskpi.(none)>
2015-04-10 08:43:09 -0400

    added closed caption video, so dulicated the play file

---
+++ startCCOMX.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+sudo killall -TERM omxplayer.bin 2>/dev/null
+sleep .1
+#cat /bin/omxfifo | omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
+#omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < cat /bin/omxfifo
+omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
