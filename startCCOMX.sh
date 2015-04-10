c06c042b7ddfa01d0d17f3c342166f5957288a0f
Garrett Martin <garrett@standalone.co>
2015-04-10 08:53:36 -0400

    Cleaning up code, inserting second video code for CC

--- startCCOMX.sh
+++ startCCOMX.sh
@@ -2,6 +2,6 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null
 sleep .1
-#cat /bin/omxfifo | omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
-#omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < cat /bin/omxfifo
-omxplayer -o local --win '282 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
+#cat /bin/omxfifo | omxplayer -o local --win '300 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
+#omxplayer -o local --win '300 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < cat /bin/omxfifo
+omxplayer -o local --win '300 0 1600 900' /media/PAGE/lineshaft_CC_1300x900.mp4
