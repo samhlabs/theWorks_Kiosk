5407364d00ef0239fd97ac0b387ecfc967237cb7
Garrett Martin <garrett@standalone.co>
2015-05-19 08:23:54 -0400

    Updating the size of the video to run in full 1920x1080

--- startCCOMX.sh
+++ startCCOMX.sh
@@ -2,4 +2,4 @@
 
 sudo killall -TERM omxplayer.bin 2>/dev/null #Kill all previously running omxplayer instances
 sleep .1 #breath... give that damn processor a break already
-omxplayer -o local --win '300 0 1600 900' /media/PAGE/lineshaft_CC_1300x900.mp4 #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
+omxplayer -o local --win '300 0 1920 1080' /media/PAGE/lineshaft_CC_1300x900.mp4 #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
