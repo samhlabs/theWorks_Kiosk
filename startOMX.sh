5241fe097c8ecf44840fbc27136be88499ec977a
Garrett Martin <garrett@standalone.co>
2015-05-26 09:59:15 -0400

    Revert "Change video to play in full 1920x1080"
    This reverts commit 99e9398289b799381485d0a7d188ca8a1936c654.

--- startOMX.sh
+++ startOMX.sh
@@ -10,4 +10,4 @@ sleep .1 #breath... give that damn processor a break already
 #omxplayer -o local --win '300 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < cat /bin/omxfifo
 #i think this is the same as above, just exhausting options
 
-omxplayer -o local --win '300 0 1920 1080' /media/PAGE/lineshaft_1300x900.mp4 #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
+omxplayer -o local --win '300 0 1600 900' /media/PAGE/lineshaft_1300x900.mp4 #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
